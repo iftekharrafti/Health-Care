accb13dd52d462c2d5bae2d33cdf93a54fe0abc9
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2021-10-19 20:26:24 +0600

    finish

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OurDepartment from '../../OurDepartment/OurDepartment';
 import Services from '../../Services/Services';
 import Banner from '../Banner/Banner';
+import Doctors from '../../Doctors/Doctors';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Services></Services>
             <OurDepartment></OurDepartment>
+            <Doctors></Doctors>
         </div>
     );
 };
